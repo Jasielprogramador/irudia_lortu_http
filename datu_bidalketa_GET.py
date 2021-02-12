d2108754dbae6e400e3c6b9c55888cbbffb940d0
Asi <asierrosa@gmail.com>
2021-02-12 09:49:22 +0100

    POST gehituta

---
+++ datu_bidalketa_GET.py
@@ -0,0 +1,31 @@
+import urllib
+
+import requests
+
+# HTTP eskaerak 4 atal ditu: metodoa, uria, goiburuak eta edukia
+#Las comillas simples y las dobles son lo mismo en python
+
+metodoa = 'GET'
+
+#Uria ez da https://ws-sendingformdata.appspot.com/html/form.html aldiz uria jakiteko "ver codigo fuente" -> "action"
+#atalean begiratu
+uria = "http://ws-sendingformdata.appspot.com/processForm"
+goiburuak = {'Host': 'ws-sendingformdata.appspot.com'}
+edukia = {'nan': '78989898'}
+
+#baina datuak nan = 79879898 formatuan bidali egin behar dira horretarako mezua parseatu egin behar dugu
+edukia_encoded = urllib.parse.urlencode(edukia)
+goiburuak['Content-Length'] = str(len(edukia_encoded))
+
+#pones esto porque en el datu eskaera de get pides el baliabide directamente desde el uri
+#adib:https://egela.ehu.eus?9878
+#el edukia es lo que le pasas tuuuuuu
+uria_datuekin = uria +'?'+edukia
+
+erantzuna = requests.request(metodoa, uria_datuekin, data=edukia, headers=goiburuak, allow_redirects=False)
+kodea = erantzuna.status_code
+deskribapena = erantzuna.reason
+print(str(kodea) + " " + deskribapena)
+
+edukia = erantzuna.content
+print(edukia)
